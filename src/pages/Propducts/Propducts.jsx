c3463522898ba1f3fedc810962ea1bd43b3ef88d
Nguyen Hoang Long <lonnguyen.030997@gmail.com>
2022-05-14 18:57:52 +0700

    v2_43

--- src/pages/Propducts/Propducts.jsx
+++ src/pages/Propducts/Propducts.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import './Products.css'
 
-export default function Propducts() {
+export default function Propducts(props) {
   return (
-    <div>Propducts</div>
+    <div className='products mt-10 mb-10 bg-black py-10'>
+      <ul className='flex flex-wrap justify-around items-center'> 
+         <li><a target="_blank" title="Bấm để mở trang trên cửa số mới" href="http://nhl-honda.surge.sh">HONDA</a></li>
+         <li><a target="_blank" title="Bấm để mở trang trên cửa số mới" href="http://nhl-myportfolio.surge.sh">MY PORTFOLIO</a></li>
+         <li><a target="_blank" title="Bấm để mở trang trên cửa số mới" href="http://nhl-meipaly.surge.sh">MEIPALY</a></li>
+         <li><a target="_blank" title="Bấm để mở trang trên cửa số mới" href="http://nhl-newshop.surge.sh">NEW SHOP</a></li>
+         <li><a target="_blank" title="Bấm để mở trang trên cửa số mới" href="http://nhl-travel.surge.sh">TRAVEL</a></li>
+         <li><a target="_blank" title="Bấm để mở trang trên cửa số mới" href="http://nlh-stalwart.surge.sh">STALWART</a></li>
+         <li><a target="_blank" title="Bấm để mở trang trên cửa số mới" href="http://nhl-mastery.surge.sh">MASTERY</a></li>
+         <li><a target="_blank" title="Bấm để mở trang trên cửa số mới" href="http://nhl-camping.surge.sh">CAMPING</a></li>
+      </ul>      
+    </div>
   )
 }
